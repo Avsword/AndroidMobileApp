e46edfd1873e7f1a841992a1d9cdb1c18ee304ed
Aaro Varjonen <90607504+Avsword@users.noreply.github.com>
2023-05-10 22:38:22 +0300

    :children_crossing: Removed the action bar

--- AndroidAPP/app/src/main/java/fi/tuni/androidapp/MainActivity.kt
+++ AndroidAPP/app/src/main/java/fi/tuni/androidapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package fi.tuni.androidapp
 
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Hide the action bar
+        supportActionBar?.hide()
 
         val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView);
         val GetAllView = GetAllView()
